8e44cfc8ca285dc85ab4de9c54d8d2fd7166a5e7
lastavchuk <it.lastavchuk@gmail.com>
2023-10-28 10:53:36 +0300

    fix: updated route

--- routes/api/quizzes.js
+++ routes/api/quizzes.js
@@ -2,34 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 const ctrl = require("../../controllers/quizzes");
-const schemas = require("../../schemas/auth");
-const { validateBody, authenticate, upload } = require("../../middlewares");
+const schemas = require("../../schemas/quizzes");
+const {
+    validateBody,
+    authenticate,
+    isValidQuizId,
+    upload,
+} = require("../../middlewares");
 
-router.post("/", authenticate, ctrl.addQuiz);
+router.post(
+    "/",
+    authenticate,
+    // validateBody(schemas.quizSchemaJoi),
+    ctrl.addQuiz
+);
 
-// router.post(
-//     "/register",
-//     validateBody(schemas.registerSchemaJoi),
-//     ctrl.register
-// );
-
-// router.get("/verify/:verificationToken", ctrl.verifyEmail);
-
-// router.post(
-//     "/verify",
-//     validateBody(schemas.emailSchemaJoi),
-//     ctrl.resendVerifyToken
-// );
-
-// router.get("/current", authenticate, ctrl.current);
-
-// router.post("/logout", authenticate, ctrl.logout);
-
-// router.patch(
-//     "/avatars",
-//     authenticate,
-//     upload.single("avatar"),
-//     ctrl.updateAvatar
-// );
+router.put(
+    "/:quizId",
+    authenticate,
+    isValidQuizId,
+    // validateBody(schemas.quizSchemaJoi),
+    ctrl.updateQuiz
+);
 
 module.exports = router;
