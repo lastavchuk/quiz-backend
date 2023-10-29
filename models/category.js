3d6ce466a03b1081276a8b8cf719ba0e91e27b21
lastavchuk <it.lastavchuk@gmail.com>
2023-10-29 14:12:21 +0200

    fix: updated validation and schemas

--- models/category.js
+++ models/category.js
@@ -1,24 +1,25 @@
 const { Schema, model } = require("mongoose");
 const errMsg = require("../constants/errors");
+const conctants = require("../constants/constants");
 const handleSaveError = require("../helpers/handleSaveError");
 
 const categorySchemaMongoose = new Schema(
-  {
-    categoryName: {
-      type: String,
-      minlength: [3, errMsg.errFieldMinLength("Category name", 3)],
-      maxlength: [50, errMsg.errFieldMaxLength("Category name", 50)],
-      required: [true, errMsg.errFieldIsrequired("Category name")],
+    {
+        categoryName: {
+            type: String,
+            minlength: [3, errMsg.errFieldMinLength("Category name", 3)],
+            maxlength: [50, errMsg.errFieldMaxLength("Category name", 50)],
+            required: [true, errMsg.errFieldIsrequired("Category name")],
+        },
+        categoryType: {
+            type: String,
+            enum: conctants.audienceArr,
+            required: [true, errMsg.errFieldIsrequired("Category type")],
+            default: conctants.audienceArr[0],
+        },
     },
-    categoryType: {
-      type: String,
-      enum: ["adults", "children"],
-      required: [true, errMsg.errFieldIsrequired("Category type")],
-      default: "adults",
-    },
-  },
 
-  { versionKey: false, timestamps: true }
+    { versionKey: false, timestamps: true }
 );
 
 // for error add
