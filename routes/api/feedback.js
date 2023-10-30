b4ac7fb72eb1224966dafa664906af90509f161a
lastavchuk <it.lastavchuk@gmail.com>
2023-10-30 10:42:15 +0200

    fix: updates import

--- routes/api/feedback.js
+++ routes/api/feedback.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../../controllers/feedback');
-const schemas = require('../../schemas/feedback');
-const { validateBody, authenticate } = require('../../middlewares');
+const router = require("express").Router();
 
-router.post('/', authenticate, validateBody(schemas.feedbackSchemaJoi), ctrl.addFeedback); //роут для авторизованих для відгуку про додаток
+const ctrl = require("../../controllers/feedback");
+const schemas = require("../../schemas/feedback");
+const { validateBody, authenticate } = require("../../middlewares");
 
-router.post('/:quizId', validateBody(schemas.feedbackSchemaJoi), ctrl.addFeedback); //роут для всіх
+router.post(
+    "/",
+    authenticate,
+    validateBody(schemas.feedbackSchemaJoi),
+    ctrl.addFeedback
+); //роут для авторизованих для відгуку про додаток
 
-router.get('/', ctrl.getAllFeedbacks);
+router.post(
+    "/:quizId",
+    validateBody(schemas.feedbackSchemaJoi),
+    ctrl.addFeedback
+); //роут для всіх
 
+router.get("/", ctrl.getAllFeedbacks);
 
 module.exports = router;
