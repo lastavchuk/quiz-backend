5f57a39616f2c970850f0155a1e73c945df74a60
Viacheslav Shylko <116965660+Laslold@users.noreply.github.com>
2023-11-03 16:05:21 +0200

    fix passedQuz edit pagination

--- controllers/quizzes.js
+++ controllers/quizzes.js
@@ -256,6 +256,10 @@ const getRandomQuizzes = async (req, res) => {
 };
 /* to quizzes controllers */
 const getPassedQuizzes = async (req, res) => {
+  const { page = 1, limit = 6 } = req.query;
+
+  const skip = (page - 1) * limit;
+  const options = { skip, limit };
   const { _id } = req.user;
 
   const { passedQuizzes } = await User.findOne(_id);
@@ -267,7 +271,7 @@ const getPassedQuizzes = async (req, res) => {
   if (passedQuizzes.length === 0) {
     return res.json([]);
   }
-  const result = await Quiz.find({ _id: { $in: idArray } })
+  const result = await Quiz.find({ _id: { $in: idArray } }, '', options)
     .populate('quizCategory', '-_id categoryName')
     .sort('-createdAt');
 
